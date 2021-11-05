b517ac1afaf5e203f7e368622c2713a4a926a00f
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-11-05 23:36:02 +0800

    react stateful experiments

--- src/components/LayoutBuilder.js
+++ src/components/LayoutBuilder.js
@@ -1,36 +1,39 @@
 import { Layout } from '@douyinfe/semi-ui';
 
-import ProfileCard from "./ProfileCard";
-import Navbar from "./layout/Navbar";
-import Foot from "./layout/Foot";
-import { Component } from "react";
-
+import ProfileCard from './ProfileCard';
+import Navbar from './layout/Navbar';
+import Foot from './layout/Foot';
+import { Component } from 'react';
+import Clock from './Clock';
 
 class LayoutBuilder extends Component {
-    render() {
-        const { Header, Footer, Content } = Layout;
-        return (
-            <Layout style={ {
-                display: 'flex',
-                border: '1px solid var(--semi-color-border)',
-                height: '100%'
-            } }>
-                <Header>
-                    <Navbar/>
-                </Header>
-                <Content
-                    style={
-                        { padding: '30px' } // align...
-                    }
-                >
-                    <ProfileCard/>
-                </Content>
-                <Footer>
-                    <Foot/>
-                </Footer>
-            </Layout>
-        )
-    }
+  render() {
+    const { Header, Footer, Content } = Layout;
+    return (
+      <Layout
+        style={{
+          display: 'flex',
+          border: '1px solid var(--semi-color-border)',
+          height: '100%'
+        }}
+      >
+        <Header>
+          <Navbar />
+        </Header>
+        <Content
+          style={
+            { padding: '30px' } // align...
+          }
+        >
+          <ProfileCard />
+          <Clock />
+        </Content>
+        <Footer>
+          <Foot />
+        </Footer>
+      </Layout>
+    );
+  }
 }
 
 export default LayoutBuilder;
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,32 +1,50 @@
-import { Skeleton } from "@douyinfe/semi-ui";
-import { Component } from "react";
+import { Skeleton } from '@douyinfe/semi-ui';
+import { Component } from 'react';
 
 class ProfileCard extends Component {
-    render() {
-        let isLoading = true;
-        // setTimeout(() => {
-        //     isLoading = false
-        // }, 3000);
-        return (
-            <div
-                style={ { backgroundColor: 'var(--semi-color-bg-1)' } }
-                className={ "profile-card" }>
-                <div
-                    style={ {
-                        borderRadius: '10px',
-                        border: '1px solid var(--semi-color-border)',
-                        height: '376px',
-                        padding: '32px'
-                    } }
-                >
-                    <Skeleton placeholder={ (<Skeleton.Paragraph rows={ 2 }/>) } loading={ isLoading }>
-                        <p>Hi, Bytedance dance dance.</p>
-                        <p>Hi, Bytedance dance dance.</p>
-                    </Skeleton>
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.fakeLoader = setTimeout(() => this.fakeLoading(), 3000);
+    console.log('fake loading is loading now');
+    console.log('is loading: ', this.state.isLoading);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.fakeLoader);
+  }
+
+  fakeLoading() {
+    this.setState({
+      isLoading: false
+    });
+    console.log('fake loading done\nis loading', this.state.isLoading);
+  }
+
+  render() {
+    return (
+      <div style={{ backgroundColor: 'var(--semi-color-bg-1)' }} className={'profile-card'}>
+        <div
+          style={{
+            borderRadius: '10px',
+            border: '1px solid var(--semi-color-border)',
+            height: '376px',
+            padding: '32px'
+          }}
+        >
+          <Skeleton placeholder={<Skeleton.Paragraph rows={2} />} loading={this.state.isLoading}>
+            <p>Hi, Bytedance dance dance.</p>
+            <p>Hi, Bytedance dance dance.</p>
+          </Skeleton>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProfileCard;
