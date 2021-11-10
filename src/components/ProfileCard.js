b4e10db8ace3ad5f51597cb0617917d44b484b25
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-11-10 17:38:45 +0800

    FE BE com test

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,16 +1,17 @@
-import { Skeleton } from '@douyinfe/semi-ui';
+import { Button, Skeleton, TextArea } from '@douyinfe/semi-ui';
 import { Component } from 'react';
-
+import axios from 'axios';
 class ProfileCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
+      isLoading: true,
+      text: String
     };
   }
 
   componentDidMount() {
-    this.fakeLoader = setTimeout(() => this.fakeLoading(), 3000);
+    this.fakeLoader = setTimeout(() => this.fakeLoading(), 1000);
     console.log('fake loading is loading now');
     console.log('is loading: ', this.state.isLoading);
   }
@@ -26,6 +27,21 @@ class ProfileCard extends Component {
     console.log('fake loading done\nis loading', this.state.isLoading);
   }
 
+  axiosGet() {
+    axios
+      .get('//localhost:8080/stupid_demo_war_exploded/xmu')
+      .then((res) => {
+        // console.log(res);
+        this.setState({
+          text: res.data
+        });
+        console.log(this.state.text);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div style={{ backgroundColor: 'var(--semi-color-bg-1)' }} className={'profile-card'}>
@@ -38,8 +54,10 @@ class ProfileCard extends Component {
           }}
         >
           <Skeleton placeholder={<Skeleton.Paragraph rows={2} />} loading={this.state.isLoading}>
-            <p>Hi, Bytedance dance dance.</p>
-            <p>Hi, Bytedance dance dance.</p>
+            <h1>SWE306 test</h1>
+            <p>abaaba</p>
+            <Button onClick={() => this.axiosGet()}>axios get</Button>
+            <TextArea value={this.state.text} />
           </Skeleton>
         </div>
       </div>
