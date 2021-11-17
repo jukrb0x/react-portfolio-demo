db272e0f1b3a85b7ec5b64d39ac139de150bae13
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-11-17 16:40:07 +0800

    chore: add dummy data

---
+++ src/data/PersonalData.js
@@ -0,0 +1,12 @@
+const personalInformation = {
+    firstName: 'Jinbiao',
+    lastName: 'Zhang',
+    tel: '+60 6666666666',
+    email: 'jukrb0x@pm.me',
+    affiliation: 'Xiamen University',
+    location: 'Amoy, China',
+    website: 'https://wh0.is',
+    language: 'EN / ZH-CN'
+};
+
+export default personalInformation;
---
+++ src/data/WorkExperienceData.js
@@ -0,0 +1,35 @@
+const workExperienceData = [
+    {
+        id: 1,
+        title: 'Graphery Project',
+        profession: 'Front-end developer',
+        datetime: '2020.05 - Now',
+        description: 'Graphery is an algorithm visualization webservice for learning Computational Biology. ' +
+            'The front-end is implemented with Vue.js + Quasar, the back-end is implemented with Python + Django + PostgreSQL, I mainly work as a UI/UX developer in this project. '
+    },
+    {
+        id: 2,
+        title: 'JB Technology',
+        profession: 'Front-end developer',
+        datetime: '2020.08 - 2020.10',
+        description: 'Daily work related to interface development. ' +
+            'Such as WeChat mini program (uni-app + PHP), Company Website (Nuxt.js), Middle Monitor Platform (Electron + Vue.js + Element UI + Spring Boot)'
+    },
+    {
+        id: 3,
+        title: 'CERN',
+        profession: 'Nucleic Physics Researcher',
+        datetime: '1954-1977',
+        description: 'Working with Richerd Feynmen, at CERN, the European Organization for Nuclear Research, is one of the world\'s largest and most respected centres for scientific research.'
+    },
+    {
+        id: 4,
+        title: 'Appla Inc.',
+        profession: 'Co-founder',
+        datetime: '1923-1941',
+        description: 'I created this multinational technology company that specializes in consumer electronics with Stewe Jabs.'
+    },
+
+];
+
+export default workExperienceData;
