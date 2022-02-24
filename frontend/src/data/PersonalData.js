069d361bce4cc5d95fedef73949b8d8e7f90ddf4
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-02-24 17:57:25 +0800

    minor changes

---
+++ frontend/src/data/PersonalData.js
@@ -0,0 +1,12 @@
+const personalInformation = {
+  firstName: 'James',
+  lastName: 'Bond',
+  tel: '+60 6666666666',
+  email: 'jukrb0x@pm.me',
+  affiliation: 'MIT',
+  location: 'Amoy, China',
+  website: 'https://wh0.is',
+  language: 'EN / ZH-CN'
+};
+
+export default personalInformation;
