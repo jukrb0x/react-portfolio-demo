1bb8a6bd956b0e0e736faf2deb5ed2cf78c9ff0b
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-02-24 18:03:01 +0800

    minor changes

---
+++ frontend/src/data/PublicationData.js
@@ -0,0 +1,32 @@
+const PublicationData = [
+    {
+        id: 1,
+        title: 'SOMETHING: interactive tutorials for SOMETHING',
+        description: 'the desc',
+        // reference will be rendered dangerously as html
+        reference: 'Joes Tribiani <b>James Bond</b>, SOMETHING: interactive tutorials for SOMETHING, <i>Physics Review Letter, Volume 49, Issue W1, 2 July 2021, Pages W257–W262, https://doi.org/10</i>'
+    },
+    {
+        id: 2,
+        title: 'Theory of Relativity',
+        description: '',
+        // reference will be rendered dangerously as html
+        reference: 'I am just kidding'
+    },
+    {
+        id: 3,
+        title: 'Quantum Mechanics: A great short story',
+        description: '',
+        // reference will be rendered dangerously as html
+        reference: 'The book talked about Quantum Mechanics in shape of its history, I am just kidding as well.'
+    },
+    {
+        id: 4,
+        title: 'Designing philosophy in Computer Science',
+        description: '',
+        // reference will be rendered dangerously as html
+        reference: 'It could be my publication in 200 years, talking about the design and the computers.'
+    },
+]
+
+export default PublicationData;
